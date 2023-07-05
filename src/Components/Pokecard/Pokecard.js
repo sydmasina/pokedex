ac28d5ac1c67b08291446ba9493c7127eb2c22b6
Sydwell Masina <sydwell.masina@digitaltwin.digital>
2023-07-05 08:29:51 +0200

    Add pokecard component

---
+++ src/Components/Pokecard/Pokecard.js
@@ -0,0 +1,18 @@
+import {Component} from 'react';
+import './Pokecard.css'
+
+class Pokecard extends Component {
+    render(){
+        const {name, type, id, base_experience} = this.props;
+        return(
+            <div className='pokecard'>
+                <h1 className='pokecard-title'>{name}</h1>
+                <img src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${id}.png`}></img>
+                <p className='pokecard-content'>Type: {type}</p>
+                <p className='pokecard-content'>Exp: {base_experience}</p>
+            </div>
+        )
+    }
+}
+
+export default Pokecard;
